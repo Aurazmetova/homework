bb013f28bafdf69750431fd213d15b6a6a00fd82
Aurazmetova <82522593+Aurazmetova@users.noreply.github.com>
2021-04-21 13:39:42 +0500

    Add files via upload

---
+++ Exercyse.py
@@ -0,0 +1,11 @@
+print("Hello, I am Aygerim"*1)
+name=input("What's your name?")
+print("Nice to meet you,"+name)
+
+age=input("How old are you?")
+
+print(" You will be "+str(int(age)+1)+ " In the next year")
+grade=input("In which grade do you study?")
+
+print("You will study on "+str(int(grade)+2)+" after two years")
+print("\n \t Danana "*5)
